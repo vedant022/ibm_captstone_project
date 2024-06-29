0e7b4a3dcecea90d64179d0b2d6c1e76b19cc247
vedant022 <150889439+vedant022@users.noreply.github.com>
2024-06-29 12:43:18 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const API_URL = window.location.hostname === "localhost" ? "https://wesamlafi390-8181.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/" : "add your theia server side url";
+console.log(
+    "API_URL :",
+    API_URL
+);
